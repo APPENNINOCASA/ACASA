da554010280bf759bb724cf44c0e9a3c79402849
APPENNINOCASA <appenninocasa@gmail.com>
2025-11-05 00:18:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Appartamenti in Appennino</h1>
+        <h1>Appinapp</h1>
         <p>Sistema di Gestione Immobili</p>
       </header>
     </div>
